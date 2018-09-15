70e191f8448b891b53797e61cc3b4465f75a4340
vauhochzett <vauhochzett@users.noreply.github.com>
2018-09-15 17:38:17 +0200

    index.js: Adds react-cookie and CookiesProvider

--- challenge-app/src/index.js
+++ challenge-app/src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'
+import { CookiesProvider } from 'react-cookie';
+import { BrowserRouter } from 'react-router-dom';
 
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
     <BrowserRouter>
-    <App />
+        <CookiesProvider>
+            <App />
+        </CookiesProvider>
     </BrowserRouter>
     , document.getElementById('root'));
 registerServiceWorker();
