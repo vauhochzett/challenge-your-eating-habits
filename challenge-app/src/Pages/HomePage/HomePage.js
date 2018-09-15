c16b32b1c15fe1bf3fc3c879b8d8af641c9ce0b8
vauhochzett <vauhochzett@users.noreply.github.com>
2018-09-16 01:30:04 +0200

    HomePage: Updates challenges with better titles and img key

--- challenge-app/src/Pages/HomePage/HomePage.js
+++ challenge-app/src/Pages/HomePage/HomePage.js
@@ -11,13 +11,15 @@ class Homepage extends Component {
         privateView: true,
         challenges: [
             {
-                title: 'No Meat 4 Weeks',
+                title: 'No Meat for a week',
+                img: 'meat',
                 author: 'FL',
                 date: new Date( 'December 17, 1995 03:24:00' ),
                 description: 'Hey Valentin! Try not to eat any meat for 4 weeks. Bet you\'ll fail haha'
             },
             {
-                title: 'No Plastic Challenge',
+                title: 'Eat an insect dish',
+                img: 'grilled_crickets',
                 author: 'KO',
                 date: new Date( 'December 17, 1995 03:24:00' ),
                 description: 'Yo Valentin! I tried not to use any plastic for one whole day. Tougher than you would think!'
