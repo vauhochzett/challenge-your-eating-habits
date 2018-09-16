2587aeb7338fdeca24c7a542265cc079dbe600a8
vauhochzett <vauhochzett@users.noreply.github.com>
2018-09-16 06:40:34 +0200

    SharePopup: Reformats code

--- challenge-app/src/Pages/CreatePage/SharePopup.js
+++ challenge-app/src/Pages/CreatePage/SharePopup.js
@@ -1,60 +1,56 @@
 import React, { Component } from "react";
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import ShareIcon from '@material-ui/icons/ShareRounded'
-import {FaWhatsapp} from 'react-icons/fa';
-import {FaFacebook} from 'react-icons/fa';
-import {FaInstagram} from 'react-icons/fa';
-import {FaSnapchat} from 'react-icons/fa';
-import {FaSkype} from 'react-icons/fa';
-
-
-
+import { FaWhatsapp } from 'react-icons/fa';
+import { FaFacebook } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
+import { FaSnapchat } from 'react-icons/fa';
+import { FaSkype } from 'react-icons/fa';
 
 
 class SharePopup extends Component {
 
     state = {
         open: false
-    }
+    };
 
     handleClickOpen = () => {
-        this.setState( { open: true } );
+        this.setState({ open: true });
     };
 
     handleClose = () => {
-        this.setState( { open: false } );
+        this.setState({ open: false });
     };
 
     render() {
 
         return <div>
             <Dialog
-                open={this.state.open}
-                onClose={this.handleClose}
-                maxWidth={'md'}
-                fullWidth={true}
+                open={ this.state.open }
+                onClose={ this.handleClose }
+                maxWidth={ 'md' }
+                fullWidth={ true }
                 aria-labelledby="alert-dialog-title"
                 aria-describedby="alert-dialog-description"
             >
-                <DialogTitle id="alert-dialog-title">{"Share!"}</DialogTitle>
-                <DialogContent onClick={this.handleClose}>
+                <DialogTitle id="alert-dialog-title">{ "Share!" }</DialogTitle>
+                <DialogContent onClick={ this.handleClose }>
                     <DialogContentText id="alert-dialog-description">
-                        <FaWhatsapp size={'2em'}/>
-                        <FaFacebook size={'2em'}/>
-                        <FaInstagram size={'2em'}/>
-                        <FaSnapchat size={'2em'}/>
-                        <FaSkype size={'2em'}/>
+                        <FaWhatsapp size={ '2em' }/>
+                        <FaFacebook size={ '2em' }/>
+                        <FaInstagram size={ '2em' }/>
+                        <FaSnapchat size={ '2em' }/>
+                        <FaSkype size={ '2em' }/>
 
                     </DialogContentText>
                 </DialogContent>
             </Dialog>
 
-            <Button variant="fab" color="primary" aria-label="Add" onClick={this.handleClickOpen}>
+            <Button variant="fab" color="primary" aria-label="Add" onClick={ this.handleClickOpen }>
                 <ShareIcon/>
             </Button>
         </div>
