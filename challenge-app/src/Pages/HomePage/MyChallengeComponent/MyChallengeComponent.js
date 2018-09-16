60ff7e9308a396c055b72e03273ad4221150f37a
vauhochzett <vauhochzett@users.noreply.github.com>
2018-09-16 03:16:21 +0200

    Challenge-component: Removes separate FinishDialog component; integrates into main class

--- challenge-app/src/Pages/HomePage/MyChallengeComponent/MyChallengeComponent.js
+++ challenge-app/src/Pages/HomePage/MyChallengeComponent/MyChallengeComponent.js
@@ -16,8 +16,13 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 import Menu from "@material-ui/core/es/Menu/Menu";
 import MenuItem from "@material-ui/core/es/MenuItem/MenuItem";
 
-// Components
-import FinishDialog from './FinishDialog'
+// Dialog
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 // Images
 import grilled_crickets from '../../../img/grilled_crickets-r.jpg';
